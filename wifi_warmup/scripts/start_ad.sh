c0824d2a17aa59f67d9d37e226e8d662d8c20dbb
Piotr Gawlowicz <gawlowicz@tu-berlin.de>
2018-05-15 15:48:10 +0200

    update scripts

--- wifi_warmup/scripts/start_ad.sh
+++ wifi_warmup/scripts/start_ad.sh
@@ -1,7 +1,7 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 WISHFUL_PATH=${DIR%/*/*/*}
-OBSS_PATH=${DIR%/*}
+AD_PATH=$WISHFUL_PATH/final_showcase/ad_controller
 
 source $WISHFUL_PATH/dev/bin/activate
 cd $AD_PATH
