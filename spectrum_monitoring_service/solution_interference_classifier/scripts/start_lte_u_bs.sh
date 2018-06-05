cee68938f6de9fc4b8109e8fcee9ee4130a9bde0
Piotr Gawlowicz <gawlowicz@tu-berlin.de>
2018-06-05 15:47:13 +0200

    update ad ip address

--- spectrum_monitoring_service/solution_interference_classifier/scripts/start_lte_u_bs.sh
+++ spectrum_monitoring_service/solution_interference_classifier/scripts/start_lte_u_bs.sh
@@ -3,7 +3,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 WISHFUL_PATH=${DIR%/*/*/*/*}
 DET_PATH=$WISHFUL_PATH/final_showcase/spectrum_monitoring_service/solution_interference_classifier
 
-AD_ADDRESS='172.16.16.9'
+AD_ADDRESS='172.16.16.2'
 
 if [ -z "$1" ]
 then
